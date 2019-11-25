7a647c6bd6bfa90cb8751510b340780048b0e6b4
Steven Brannum <sdbrannum@gmail.com>
2019-11-24 21:55:43 -0600

    2.5: parsing return statements

--- ast/ast.go
+++ ast/ast.go
@@ -30,20 +30,6 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-type LetStatement struct {
-	Token token.Token // the token.LET token
-	Name  *Identifier
-	Value Expression
-}
-
-func (ls *LetStatement) statementNode() {
-
-}
-
-func (ls *LetStatement) TokenLiteral() string {
-	return ls.Token.Literal
-}
-
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
@@ -51,4 +37,29 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
--- parser/parser.go
+++ parser/parser.go
@@ -54,6 +54,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -74,6 +76,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	// TODO: we're skippibng the expressions until we
 	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: We're skipping the expression until we
+	// encounter a semilon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
